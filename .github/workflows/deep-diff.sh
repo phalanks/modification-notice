14b9472644c67062c061b8fff369a365a55b9877
Hiroshi Hayakawa <hhiroshell@gmail.com>
2024-03-08 05:37:43 +0900

    fix

--- .github/workflows/deep-diff.sh
+++ .github/workflows/deep-diff.sh
@@ -5,14 +5,14 @@
 set -e
 
 if [ "$#" -ne 3 ]; then
-    echo "The number of arguments must be 3."
-    echo "Usage: ./list-targets.sh [path] [commit-before] [commit-after]"
+    echo "The number of arguments must be 3." >&2
+    echo "Usage: ./list-targets.sh [path] [commit-before] [commit-after]" >&2
     exit 1
 fi
 
 # Differences in targets of symbolic links.
 symlinks=$(find "$1" -type l)
 for l in ${symlinks}; do
-  linktarget=$(readlink -f "${l}")
-  git diff --name-only "$2" "$3" "$(realpath --relative-to=. "${linktarget}")"
+    linktarget=$(readlink -f "${l}")
+    git diff --name-only "$2" "$3" "$(realpath --relative-to=. "${linktarget}")"
 done
